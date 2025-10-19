ee1b5dde21c0c01b8b4da937c3107e1a6ea0e236
YuanXsX <y401754242@126.com>
2025-10-19 15:43:26 +0800

    拦截器部署

--- laf_server/src/main/java/com/laf/config/WebMvcConfiguration.java
+++ laf_server/src/main/java/com/laf/config/WebMvcConfiguration.java
@@ -4,88 +4,41 @@ import com.laf.interceptor.JwtTokenAdminInterceptor;
 import com.laf.json.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
-/**
- * 配置类，注册web层相关组件
- */
 @Configuration
 @Slf4j
-public class WebMvcConfiguration extends WebMvcConfigurationSupport {
+public class WebMvcConfiguration implements WebMvcConfigurer {
 
     @Autowired
     private JwtTokenAdminInterceptor jwtTokenAdminInterceptor;
 
-    /**
-     * 注册自定义拦截器
-     *
-     * @param registry
-     */
-    protected void addInterceptors(InterceptorRegistry registry) {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
         log.info("注册自定义拦截器...");
         registry.addInterceptor(jwtTokenAdminInterceptor)
-                .addPathPatterns("/laf/user/**")
-                .excludePathPatterns("/laf/user/login");
+                .addPathPatterns("/laf/user/**") // 临时全匹配验证拦截器是否被调用
+                .excludePathPatterns("/laf/user/login", "/laf/user/register");
     }
 
-    /**
-     * 通过knife4j生成接口文档
-     * @return
-     */
-    @Bean
-    public Docket docket() {
-        log.info("开始生成测试文档");
-        ApiInfo apiInfo = new ApiInfoBuilder()
-                .title("失物招领接口文档")
-                .version("2.0")
-                .description("失物招领项目接口文档")
-                .build();
-        Docket docket = new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.laf.controller"))
-                .paths(PathSelectors.any())
-                .build();
-        return docket;
-    }
-
-    /**
-     * 设置静态资源映射
-     * @param registry
-     */
-    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        log.info("设置静态资源映射...");
-        registry.addResourceHandler("/doc.html").addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }
-
-    /**
-     * 扩展Spring mvc框架消息转换器
-     * @param converters
-     */
     @Override
-    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-        log.info("开始扩展Spring mvc框架消息转换器...");
-        //添加自定义的消息转换器
-        MappingJackson2HttpMessageConverter converter= new MappingJackson2HttpMessageConverter();
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        log.info("设置静态资源映射（仅应用自有静态资源）...");
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+    }
 
-        //对消息转化器添加一个对象转化器，对象转化器可以将Java对象转换为JSON格式
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        log.info("开始扩展Spring mvc消息转换器...");
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
         converter.setObjectMapper(new JacksonObjectMapper());
-        //将自定义的消息转换器添加到Spring mvc框架中
         converters.add(0, converter);
     }
 }
